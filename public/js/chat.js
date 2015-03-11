16f1d11e717c61abc8235b7671b6f898875a72e4
Neo He <popman.he@gmail.com>
2015-03-11 17:13:56 +0800

    chat enhanced

--- public/js/chat.js
+++ public/js/chat.js
@@ -2,11 +2,18 @@
     enterMsg: ko.observable(''),
     chatButtonEnable: ko.observable(1),
     sendMsg: function () {
-        socket.emit('sendChat', { msg: this.enterMsg() });
-        chatArray.push({ chatUser: 'Neo', chatTime: moment(new Date()).format('YYYY-MM-DD HH:mm'), chatMsg: this.enterMsg() });
-        this.chatButtonEnable(0);
-        //Pause 5 seconds to enter next message
-        setTimeout(function () { chatVM.chatButtonEnable(1); }, 1000 * 5);
+        if (this.enterMsg().length > 0) {
+            socket.emit('sendChat', { msg: this.enterMsg() });
+            if (chatArray().length > 100)
+                chatArray.shift();
+            chatArray.push({ chatUser: 'Neo', chatTime: moment(new Date()).format('YYYY-MM-DD HH:mm'), chatMsg: this.enterMsg() });
+            this.chatButtonEnable(0);
+            this.enterMsg('');
+            var container=$('#chatList')
+            container.animate({ scrollTop: container.height() + 20000 }, 1000);
+            //Pause 5 seconds to enter next message
+            setTimeout(function () { chatVM.chatButtonEnable(1); }, 1000 * 5);
+        }
     }
 };
 var chatArray = ko.observableArray();
@@ -14,7 +21,10 @@ var chatArray = ko.observableArray();
 $(function () {
     ko.applyBindings(chatVM, document.getElementById("chatBox"));
     socket.on('recvChat', function (result) {
+        if (chatArray().length > 100)
+            chatArray.shift();
         chatArray.push({ chatUser: 'Neo', chatTime: moment(result.chatTime).format('YYYY-MM-DD HH:mm'), chatMsg: result.chatMsg });
-       
+        var container = $('#chatList')
+        container.animate({ scrollTop: container.height() + 20000 }, 1000);
     });
 });
