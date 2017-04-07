a3e178111513561667dac4f11579bf05c942918e
neo <popman_he@hotmail.com>
2017-04-07 13:45:20 -0700

    add volumes to docker-compose

--- client/src/config/index.js
+++ client/src/config/index.js
@@ -4,7 +4,7 @@ const configureDev = {
 };
 
 const configureProd = {
-    socketUrl: 'http://' + window.location.hostname + ':4000' //assume client and server are running at the same address
+    socketUrl: 'http://' + window.location.hostname + ':4000/' //assume client and server are running at the same address
 };
 
 const config = process.env.NODE_ENV === 'production' ? configureProd : configureDev;
